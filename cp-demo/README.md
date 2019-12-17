2a29471d042de738c760940dfb8d979b16c7f646
glsnb <guoyu5212@163.com>
2019-12-17 19:26:23 +0800

    platformInit

---
+++ cp-demo/README.md
@@ -0,0 +1,32 @@
+# cp-demo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 创建平台项目
+[构建文档](https://json-drived-configurable-platform.github.io/cp/#/doc/start)
---
+++ cp-demo/src/main.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import iView from "iview";
+import VueRouter from "vue-router";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "iview/dist/styles/iview.css";
+import "form-generator-iview/src/style/index.less";
+import "data-vis-iview/src/style/index.less";
+import FormGenerator from "form-generator-iview";
+import DataVis from "data-vis-iview";
+
+Vue.use(VueRouter);
+Vue.use(iView);
+
+// 开启debug模式
+Vue.config.debug = true;
+
+Vue.use(FormGenerator);
+Vue.use(DataVis);
+new Vue({
+  el: "#app",
+  router,
+  store,
+  render: h => h(App)
+});
